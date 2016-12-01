018567e2903c384c9dbd2cc483669997ff9ff0ba
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-01 14:28:44 +0100

    remove secrets from config
    also all used secrets have been revoked

--- config/dev.exs
+++ config/dev.exs
@@ -36,7 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure 3rd parties
 config :digitalocean_connector,
   dnsimple_client_id: "bbf472f1dcc6d611",
-  dnsimple_client_secret: "JHIKTQjbzCtoBlf0TKpvE1B7jj0DzdrZ",
   digitalocean_client_id: "a5112b0d1ca19f0b01027e87969c778dc9213fe9da2f834a0abcfc28d568f8b7",
-  digitalocean_callback_url: "http://localhost:4000/digitalocean/callback",
-  digitalocean_client_secret: "344363138b6bc11173c19e649c43a0330526a5b25debaa426050527a7ad5793c"
+  digitalocean_callback_url: "http://localhost:4000/digitalocean/callback"
+
+# Finally import the config/dev.secret.exs
+import_config "dev.secret.exs"
