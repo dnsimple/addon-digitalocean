a09c18ac05adb76836ea8ce8b79d02fea05ee93b
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-14 12:10:24 +0100

    update dnsimple client to 1.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule DigitalOceanConnector.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:dnsimple, git: "https://github.com/dnsimple/dnsimple-elixir.git"},
+     {:dnsimple, "~> 1.0"},
      {:httpoison, "~> 0.10.0"}]
   end
 
